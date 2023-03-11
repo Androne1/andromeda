279898e8d70470ca90ec4f2d68310ef7fadb68c6
Michael Faradei <faradei@mail.ru>
2023-03-11 17:25:41 +0300

    Roman removed all trash and add READme, requirements and gitignore

--- 02_cows_and_bulls/config.py
+++
@@ -1,6 +0,0 @@
-TOKEN = '5821885871:AAHMbgM5Cexdvwxxy3I3SC7oIsYBS_T4ODU'
-# with open('02_cows_and_bulls/easy.txt', encoding='utf-8') as file:
-#     words = file.read()
-# words = words.replace(', ', '\n')
-# with open('02_cows_and_bulls/easy.txt', 'w', encoding='utf-8') as file:
-#     file.write(words)
---
+++ READme.md
@@ -0,0 +1,4 @@
+# Telegram bots
+
+Чтобы пользоваться, введите в терминал команду pip install -r requirements.txt
+Затем добавьте config.py с токеном в папку с ботом
