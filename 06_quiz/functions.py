2ec73365bfed7225040ab73020aee613edfce973
Michael Faradei <faradei@mail.ru>
2023-03-25 19:33:28 +0300

    Я закончил работу над викториной

--- 06_quiz/functions.py
+++ 06_quiz/functions.py
@@ -6,11 +6,11 @@ from telegram import (
 )
 import csv
 import random
+import time
 
 GO = 'Вперёд'
-QUESTION_COUNT = 10
+QUESTION_COUNT = 3
 GAME, FINISH = 1, 2
-RIGHT_ANSWER_COUNT = 0
 
 def read_csv():
     with open('06_quiz\questions.csv', 'r', encoding = 'utf-8') as file:
@@ -33,32 +33,41 @@ def start(update: Update, context: CallbackContext):
         input_field_placeholder = f'Нажми на кнопку "{GO}", поиграем!'
     )
     update.message.reply_text('Добро пожаловать в викторину, вам будет показан один вопрос и 4 варианта ответа на него, среди которых есть только один правильный, ваша задача выбрать его')
-    update.message.reply_text(f'Если хотите начать игру, то нажмите кнопку{GO}', reply_markup = keyboard)
+    update.message.reply_text(f'Если хотите начать игру, то нажмите кнопку {GO}', reply_markup = keyboard)
     questions = read_csv()
     random.shuffle(questions)
-    questions = questions[QUESTION_COUNT:]
+    questions = questions[:QUESTION_COUNT]
     context.user_data['вопросы'] = questions
+    context.user_data['right'] = 0
+    context.user_data['right_answer'] = GO
     return GAME
     
 
 def game(update: Update, context: CallbackContext):
-    if QUESTION_COUNT <= 0:
+    user_answer = update.message.text
+    right_answer = context.user_data['right_answer']
+    print(f'{user_answer} it is mine, {right_answer} - right answer')
+    if user_answer == right_answer:
+        context.user_data['right'] += 1
+    if len(context.user_data['вопросы']) == 0:
+        RIGHT_ANSWER_COUNT = context.user_data['right']-1
         update.message.reply_text(f'Вы ответили на {RIGHT_ANSWER_COUNT} вопросов')
         if RIGHT_ANSWER_COUNT == 0:
             update.message.reply_text('Вам надо тренироваться')
-        elif RIGHT_ANSWER_COUNT <= 4 & RIGHT_ANSWER_COUNT != 0:
+        elif RIGHT_ANSWER_COUNT <= 4 and RIGHT_ANSWER_COUNT != 0:
             update.message.reply_text('Вы можете лучше')
-        elif RIGHT_ANSWER_COUNT >= 4 & RIGHT_ANSWER_COUNT <= 6:
-            update.message.reply_text('Неплохо, почти половина ответов были правильными')
-        elif RIGHT_ANSWER_COUNT >= 6 & RIGHT_ANSWER_COUNT <= 9:
+        elif RIGHT_ANSWER_COUNT > 4 and RIGHT_ANSWER_COUNT <= 6:
+            update.message.reply_text('Неплохо, +- половина ответов были правильными')
+        elif RIGHT_ANSWER_COUNT > 6 and RIGHT_ANSWER_COUNT <= 9:
             update.message.reply_text('Очень хорошо, вы были недалеки от идеала')
         elif RIGHT_ANSWER_COUNT == 10:
             update.message.reply_text('Идеально!')
-            return ConversationHandler.END
+        return ConversationHandler.END
     questions = context.user_data['вопросы']
     answer = questions.pop()
     question_text = answer.pop(0)
     right_answer = answer[0]
+    context.user_data['right_answer'] = right_answer
     random.shuffle(answer)
     mark_up = [answer[2:], answer[:2]]
     keyboard = ReplyKeyboardMarkup(
@@ -66,23 +75,11 @@ def game(update: Update, context: CallbackContext):
         resize_keyboard = True
         )
     update.message.reply_text(question_text, reply_markup = keyboard)
-    QUESTION_COUNT -= 1
-    if update.message.text == right_answer:
-        RIGHT_ANSWER_COUNT += 1
+    # right_answer_count = context.user_data['right']
+    # QUESTION_COUNT -= 1
     
-    if QUESTION_COUNT == 0:
-        update.message.reply_text(f'Вы ответили на {RIGHT_ANSWER_COUNT} вопросов')
-        if RIGHT_ANSWER_COUNT == 0:
-            update.message.reply_text('Вам надо тренироваться')
-        elif RIGHT_ANSWER_COUNT <= 4 & RIGHT_ANSWER_COUNT != 0:
-            update.message.reply_text('Вы можете лучше')
-        elif RIGHT_ANSWER_COUNT >= 4 & RIGHT_ANSWER_COUNT <= 6:
-            update.message.reply_text('Неплохо, почти половина ответов были правильными')
-        elif RIGHT_ANSWER_COUNT >= 6 & RIGHT_ANSWER_COUNT <= 9:
-            update.message.reply_text('Очень хорошо, вы были недалеки от идеала')
-        elif RIGHT_ANSWER_COUNT == 10:
-            update.message.reply_text('Идеально!')
-            return ConversationHandler.END
+    
+    
     
     
 def end(update: Update, context: CallbackContext):
